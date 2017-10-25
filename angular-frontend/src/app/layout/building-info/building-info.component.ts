69ff0f9d6563f7ca71ba047e9c2253acb9e44a90
Héctor Bállega <hectorballega@gmail.com>
2017-10-25 16:53:29 +0200

    Added Building Information View

---
+++ angular-frontend/src/app/layout/building-info/building-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-building-info',
+  templateUrl: './building-info.component.html',
+  styleUrls: ['./building-info.component.scss']
+})
+export class BuildingInfoComponent implements OnInit {
+  constructor() {
+  }
+  ngOnInit() {
+  }
+
+}
